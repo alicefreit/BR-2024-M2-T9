5a76877eb6366ab15cc11a6a44e403506aa81777
alicefreit <mariaalicefreitascardim@gmail.com>
2023-10-20 23:29:39 -0300

    Menu, score e death_count

--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -18,4 +18,3 @@ class Obstacle(Sprite):
 
     def draw(self, screen):
         screen.blit(self.image[self.type], (self.rect.x, self.rect.y))
-
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -9,29 +9,26 @@ class ObstacleManager:
     def __init__(self):
         self.obstacles = []
 
-    def update(self, game): 
-        obstacle_type = [ #chama os cactus e os passaros
+    def update(self, game):
+        obstacle_type = [
             Cactus(),
             Bird(),
         ]
 
-        if len(self.obstacles) == 0:            
-            self.obstacles.append(obstacle_type[random.randint(0,1)]) # fala para os obstaculos aparecenrem de forma aleatoria
-            
+        if len(self.obstacles) == 0:
+            self.obstacles.append(obstacle_type[random.randint(0, 1)])
+
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                if not game.player.has_power_up: # enquanto um obstaculo estiver aparecendo. Se o jogador bater no obstáculo, o jogo acabe e fecha
-                    pygame.time.delay(500)
-                    game.playing = False
-                    game.death_count += 1
-                    break
-                else: # Se não a imagem sai e o jogo continua
-                    self.obstacles.remove(obstacle)
+                pygame.time.delay(500)
+                game.playing = False
+                game.death_count += 1
+                break
 
     def reset_obstacles(self):
-        self.obstacles = [] 
+        self.obstacles = []
 
-    def draw(self, screen):
-        for obstacle in self.obstacles: # para um obstaculo em "self.obstacles"
-            obstacle.draw(screen) # o desenho passa de forma continua
+    def draw (self, screen):
+        for obstacle in self.obstacles:
+            obstacle.draw(screen)
